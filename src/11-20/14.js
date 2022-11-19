97b0c4a3d7a93b045cd3d4de697ebfb9055bf800
iknil <lk.nil@outlook.com>
2022-11-19 18:03:00 +0800

    fest: finish 14

---
+++ src/11-20/14.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+	const LEN = strs.length;
+	if (LEN === 1) {
+		return strs[0];
+	}
+
+	let i = 0;
+	let result = '';
+	while (true) {
+		let j = 1;
+		for (; j < LEN; j += 1) {
+			if (strs[j][i] === undefined
+				|| strs[j - 1][i] === undefined
+				|| strs[j][i] !== strs[j - 1][i]) {
+				break;
+			}
+		}
+		if (j === LEN) {
+			result += strs[0][i];
+		} else {
+			break;
+		}
+		i += 1;
+	}
+	return result;
+}
+
+console.log(longestCommonPrefix(["", ""]));
