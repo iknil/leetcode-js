3e50c8b096d31e0645b24e783031ae804fab5c6b
iknil <lk.nil@outlook.com>
2022-11-26 20:20:22 +0800

    style(src): beautify code 21

--- src/21-30/21.js
+++ src/21-30/21.js
@@ -7,15 +7,17 @@
  */
 
 function mergeTwoLists(list1, list2) {
-	if (!list1) return list2;
-	if (!list2) return list1;
+	const l1 = list1;
+	const l2 = list2;
+	if (!l1) return l2;
+	if (!l2) return l1;
 
-	if (list1.val > list2.val) {
-		list2.next = mergeTwoLists(list1, list2.next);
-		return list2;
+	if (l1.val > l2.val) {
+		l2.next = mergeTwoLists(l1, l2.next);
+		return l2;
 	}
-	list1.next = mergeTwoLists(list1.next, list2);
-	return list1;
+	l1.next = mergeTwoLists(l1.next, l2);
+	return l1;
 }
 
 console.log(mergeTwoLists);
